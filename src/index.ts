6b945a395af54a5dc70d1a7e169f005c44d49949
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2026-04-21 20:51:05 +0530

    init project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-import User from "./classes/User.js";
-
-// create the first user
-const user1 = new User("John Doe", 30);
-console.log(user1);
-
-// clone the first user to create a second user and change the fields of the second user as we want without affecting the first user
-const user2 = user1.clone();
-user2.name = "Jeral Sandeeptha";
-user2.age = 25;
-console.log(user2);
+console.log('App is working');
